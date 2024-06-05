009eeb4c5561a761103dcea19966ea8d4e260880
winniegithubrit <winnie.jomo@student.moringaschool.com>
2024-06-05 16:21:41 +0300

    get payment status in progress

--- BankSystem/Controllers/MTNMomoController.cs
+++ BankSystem/Controllers/MTNMomoController.cs
@@ -57,7 +57,6 @@ namespace BankSystem.Controllers
       try
       {
         var result = await _mtnMomoService.CreatePaymentAsync(model);
-        // Assuming CreatePaymentAsync returns the ID or some result after storing data in the database
         return Ok(result);
       }
       catch (Exception ex)
@@ -71,20 +70,15 @@ namespace BankSystem.Controllers
     {
       try
       {
-        // Replace 'accessToken', 'targetEnvironment', and 'subscriptionKey' with actual values
         string accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6IjRhYmQ5Y2YzLTk4YTUtNDhmMy1iMmZhLWJkNmIzMjZjYjYzNSIsImV4cGlyZXMiOiIyMDI0LTA2LTA1VDEyOjM3OjQ4LjY2MCIsInNlc3Npb25JZCI6Ijk3YWZlYzdhLTk3YmYtNGNjZS05OTI2LWI5NGUyY2Q4MGU2ZSJ9.YdgMAGLGMjwISTI-nSxmxaluv7k5ZiNOimMvahx3qqaUrbtRNKZmNEZcNpDzBBw-otlEKTzKX3VD9pb9paZWQ2GmcTXVDIEx5fTEM0NXpqYJFiVcug_tZzZD3N4eP4I_IYKUJQFPeJ0JxjvkTu6EOKsa7pmVmd7hP6Am4-QB3TiTK1H3W2gNnd6AQyRn4wnbx5ko2t7LZjaAwwN8bzD3LPxD6rTIzebrIJXj5ciOqmWZn6YLHVpq2s3seT3kS-K8qc4cqXwX9v5Lrc7x0LT5Bc2SKAfgcrVjaPsko23up8JvqrNTygTx7w75wXLWsYbhuQGztluCr0cKH0oQQIdiiA";
         string targetEnvironment = "sandbox";
         string subscriptionKey = "184789bdc53f4c05870da82d1c307b63";
 
-        // Call the service to get the account balance
         var result = await _mtnMomoService.GetAccountBalanceAsync(accessToken, targetEnvironment, subscriptionKey);
-
-        // Return the account balance as JSON
         return Ok(result);
       }
       catch (Exception ex)
       {
-        // Handle any errors and return an error response
         return StatusCode(500, $"An error occurred while fetching account balance: {ex.Message}");
       }
     }
@@ -97,24 +91,15 @@ namespace BankSystem.Controllers
         {
           return BadRequest("Currency parameter is required.");
         }
-
-        // Replace 'accessToken', 'targetEnvironment', and 'subscriptionKey' with actual values
         string accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6IjRhYmQ5Y2YzLTk4YTUtNDhmMy1iMmZhLWJkNmIzMjZjYjYzNSIsImV4cGlyZXMiOiIyMDI0LTA2LTA1VDEyOjU3OjU4LjA1MyIsInNlc3Npb25JZCI6IjdlZDNkNzRhLWZlNDQtNDBhMy1hNDdlLTdlYTg2NzkzOTU0MiJ9.FDLjRwWV-pYa5Mn3AS6nXDsc_YrJ8h4zyM0S14-tLTR-AKCR2vt-I0S0vt7oww5INpVHiMFue5wMWkyEOWpo2kp1Lqs3yJCmJAStuBfPC_1y4Xp_7ixC5wlcmkdVcgSmI85swu_t0wFpzTB_jdFdy7d1W7NQKn_H0dFAqdyi7o_YfRyS52f0sVDJEtQh0mV1tSiFI4w9w4xSr-HqoOjfbNp15yHVOvuIQpz-jgCm9Wb5_bLFoZmd8TIIcngYV9RQ1ZSkoMkMcljYObJRwwBaLlf-mH_5WRyFBWMHAAAkLQWU8a9U1rGIEnzmmcLJQoggP1RQklUwxBLIYebpO6WP9A";
         string targetEnvironment = "sandbox";
         string subscriptionKey = "184789bdc53f4c05870da82d1c307b63";
-
-        // Call the service to get the account balance in the specified currency
         var result = await _mtnMomoService.GetAccountBalanceInSpecificCurrencyAsync(accessToken, targetEnvironment, subscriptionKey, currency);
-
-        // Return the account balance as JSON
         return Ok(result);
       }
       catch (HttpRequestException ex)
       {
-        // Log the exception
         _logger.LogError($"HTTP request failed: {ex.Message}");
-
-        // Handle specific HTTP request exceptions
         if (ex.StatusCode.HasValue)
         {
           return StatusCode((int)ex.StatusCode, ex.Message);
@@ -126,10 +111,7 @@ namespace BankSystem.Controllers
       }
       catch (Exception ex)
       {
-        // Log the exception
         _logger.LogError($"An error occurred: {ex.Message}");
-
-        // Handle any other exceptions
         return StatusCode(500, $"An error occurred while fetching account balance: {ex.Message}");
       }
     }
@@ -143,24 +125,15 @@ namespace BankSystem.Controllers
         {
           return BadRequest("Account holder MSISDN parameter is required.");
         }
-
-        // Replace 'accessToken', 'targetEnvironment', and 'subscriptionKey' with actual values
         string accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6IjRhYmQ5Y2YzLTk4YTUtNDhmMy1iMmZhLWJkNmIzMjZjYjYzNSIsImV4cGlyZXMiOiIyMDI0LTA2LTA1VDEzOjMyOjI1LjA1NyIsInNlc3Npb25JZCI6IjZiZTRjOTg1LWVkOGEtNDNjYy1hYzI3LWVkOTBmM2E4Yzk3MyJ9.DIfkb-a5-Sxp0p6h0rYQLYmTKKMQZuhTZkbJ54CxEOKvwvigyinPb7QQR06HMqOtrQMErPdSYspF-dFHlSL5uGUkhLc7aCw_Cv9jrnsm-Uu1_NYHLyMpDeDbZUK-zO84jbTAEXn2KIfQhEVSsxDvUCHBD18S9xiq7LpCwbzonVQIfOhKSIuEqQyU01LhbTsNLNYHbbF_MA_hJ3UsxVqFTSbVXTOZZGPvfh8J7MT3PGMpHRlK2Yhte1rL-4edwxzLB3XOQRrCpfYYYwcA9WQkULWqDrs509hsEx_q_0GSwKA3cdqkpygxx_s1dOKonYev5D9ncuPGVm9G7MEBA-t8WA";
         string targetEnvironment = "sandbox";
         string subscriptionKey = "184789bdc53f4c05870da82d1c307b63";
-
-        // Call the service to get the basic user information
         var result = await _mtnMomoService.GetBasicUserInfoAsync(accessToken, targetEnvironment, subscriptionKey, accountHolderMSISDN);
-
-        // Return the basic user information as JSON
         return Ok(result);
       }
       catch (HttpRequestException ex)
       {
-        // Log the exception
         _logger.LogError($"HTTP request failed: {ex.Message}");
-
-        // Handle specific HTTP request exceptions
         if (ex.StatusCode.HasValue)
         {
           return StatusCode((int)ex.StatusCode, ex.Message);
@@ -172,14 +145,46 @@ namespace BankSystem.Controllers
       }
       catch (Exception ex)
       {
-        // Log the exception
         _logger.LogError($"An error occurred: {ex.Message}");
-
-        // Handle any other exceptions
         return StatusCode(500, $"An error occurred while fetching basic user information: {ex.Message}");
       }
     }
 
+    [HttpGet("payment-status/{xReferenceId}")]
+    public async Task<IActionResult> GetPaymentStatus(string xReferenceId)
+    {
+      try
+      {
+        if (string.IsNullOrWhiteSpace(xReferenceId))
+        {
+          return BadRequest("X-Reference-Id parameter is required.");
+        }
+
+       
+        string accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6IjRhYmQ5Y2YzLTk4YTUtNDhmMy1iMmZhLWJkNmIzMjZjYjYzNSIsImV4cGlyZXMiOiIyMDI0LTA2LTA1VDEzOjU2OjE1Ljc1MiIsInNlc3Npb25JZCI6IjE5MzNhOGE4LTM5ODYtNDIzMy05MzE0LWM2MTQ5YTIzNTBlMiJ9.fZrmBmWiPWGz4II19hgGf5iqiFN3jqDaIgJrsgPGfUODAFWEwzapLk5mIA5etFHViN4ugFFAT5PTq9B7ZRngoIQm3Qw8s6fh0h4lmv3_0QpERfR1URNw6b5LEaBYTjPiDM_Kv2eaN3qJjYmdev7weM2JywGuYpOxCRsHPVJP5DbeuZW9_V12XViln1Uanik3kOuw6Yq84RAoVjAKQ7Mpji0IjXKf8Xf6wjSfYpy6QacKFSZt0OEDaxkyPj3ttjBZ10alkkUyBeo9RdBj7YZuDgOP8O9ULLOv4fRa32M5YTgUe9Nt8vL0EWzZWUgw9g4u8w6i61e5Wld9ZT6AYrC6BQ";
+        string targetEnvironment = "sandbox";
+        string subscriptionKey = "184789bdc53f4c05870da82d1c307b63";
+        var result = await _mtnMomoService.GetPaymentStatusAsync(accessToken, targetEnvironment, subscriptionKey, xReferenceId);
+        return Ok(result);
+      }
+      catch (HttpRequestException ex)
+      {
+        _logger.LogError($"HTTP request failed: {ex.Message}");
+        if (ex.StatusCode.HasValue)
+        {
+          return StatusCode((int)ex.StatusCode, ex.Message);
+        }
+        else
+        {
+          return StatusCode(500, "An error occurred while getting payment status. Please try again later.");
+        }
+      }
+      catch (Exception ex)
+      {
+        _logger.LogError($"An error occurred: {ex.Message}");
+        return StatusCode(500, $"An error occurred while getting payment status: {ex.Message}");
+      }
+    }
 
   }
 }
--- BankSystem/obj/Debug/net8.0/BankSystem.AssemblyInfo.cs
+++ BankSystem/obj/Debug/net8.0/BankSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99cdf97152b02b02b19a35ffaa5f1b08044d1ff9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+554e0450dc8bd269aac9ad538ad357b389b7ed11")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
